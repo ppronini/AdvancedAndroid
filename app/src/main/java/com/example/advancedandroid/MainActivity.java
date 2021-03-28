711df74ca047f5dad5bebb80886e7b401dacdae2
ppronini <ppronin@gmail.com>
2021-03-28 17:52:29 +0600

    123

--- app/src/main/java/com/example/advancedandroid/MainActivity.java
+++ app/src/main/java/com/example/advancedandroid/MainActivity.java
@@ -3,6 +3,15 @@ package com.example.advancedandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +19,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView myListView=findViewById(R.id.myListView);
+
+        ArrayList<String> myFamily = new ArrayList<String>();
+
+        myFamily.add("Armana");
+        myFamily.add("Ratmir");
+        myFamily.add("Pasha");
+        myFamily.add("Yaser");
+
+
+        ArrayAdapter<String> arrayAdapter=new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,myFamily);
+
+        myListView.setAdapter(arrayAdapter);
+
+        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                Log.i("Name logges is",myFamily.get(position));
+
+                Toast.makeText(getApplicationContext(),myFamily.get(position),Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
